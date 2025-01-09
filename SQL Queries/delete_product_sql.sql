36211b0424cd327df2eca499961da3ce526231f5
Supposedsanity0 <karimalskay@gmail.com>
2025-01-09 15:27:16 +0200

    Update delete_product_sql.sql

--- SQL Queries/delete_product_sql.sql
+++ SQL Queries/delete_product_sql.sql
@@ -1,6 +1,2 @@
-DELETE FROM Customer WHERE C_O_ID IN (SELECT Order_id FROM Orders WHERE O_P_ID = '$product_id');
-
-DELETE FROM Orders WHERE O_P_ID = '$product_id';
-
 DELETE FROM Products WHERE Product_ID = '$product_id';
 
